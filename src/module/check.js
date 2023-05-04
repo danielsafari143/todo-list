1b26165b69534eef18938dd50d23fcc9ac2ffe2b
danielsafari143 <danielsafari143@gmail.com>
2023-05-04 13:00:16 +0200

    Update readme file

--- src/module/check.js
+++ src/module/check.js
@@ -1,10 +1,10 @@
-const check = (item , localValue ) => {
-    if(item.charAt(1) === 'i'){
-        const store = [...localValue]
-        let id = parseInt(item.charAt(0));
-        store[id].completed = !store[id].completed;
-        localStorage.setItem('todo' , JSON.stringify(store));
-    }
-}
+const check = (item, localValue) => {
+  if (item.charAt(1) === 'i') {
+    const store = [...localValue];
+    const id = parseInt(item.charAt(0), 10);
+    store[id].completed = !store[id].completed;
+    localStorage.setItem('todo', JSON.stringify(store));
+  }
+};
 
 export default check;
