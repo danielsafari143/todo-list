15f743ab782287fbf9b7e97d638e65a4534518e7
danielsafari143 <danielsafari143@gmail.com>
2023-05-04 02:31:13 +0200

    add update the remove file

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 import './index.css';
 import displayTask from './module/displayTask.js';
 import adding from './module/add.js';
-// import removetask from './module/remove.js';
+import removetask from './module/remove.js';
 import edit from './module/edit.js';
-
-document.addEventListener('keyup', (event) => {
-  const texte = document.getElementById(event.target.id).innerHTML;
-  edit(event.target.id, texte, JSON.parse(localStorage.getItem('todo')), displayTask);
-});
 
 displayTask(JSON.parse(localStorage.getItem('todo')));
 
-document.getElementById('btn').addEventListener('click', () => {
+document.getElementById('insertBtn').addEventListener('click', () => {
   const insertValue = document.getElementById('inputTask').value;
   return adding(insertValue, JSON.parse(localStorage.getItem('todo')), displayTask);
 });
 
-// document.getElementById('btn').addEventListener('click', () => {
-//   // const insertValue = document.getElementById('inputTask').value;
-//   removetask(4, JSON.parse(localStorage.getItem('todo')), displayTask);
-// });
+document.addEventListener('click', (event) => {
+    const ids = event.target.parentNode.id;
+    if(ids.charAt(1) === 't') removetask(parseInt(ids.charAt(0)), JSON.parse(localStorage.getItem('todo')), displayTask);
+    document.addEventListener('keyup', (event) => {
+        const texte = document.getElementById(event.target.id).innerHTML;
+        edit(event.target.id, texte, JSON.parse(localStorage.getItem('todo')), displayTask);
+      });
+  });
